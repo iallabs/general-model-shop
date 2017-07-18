d4cac88eeae387ffb85dcb226a68a439a623865b
IAL Dev <hilalyamine@gmail.com>
2017-07-18 17:22:18 +0200

    Changes

--- ecom/profiles/views.py
+++ ecom/profiles/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     context = {}
-    template = 'home.html'
+    template = 'home2.html'
     return render(request,template,context)
 
 def about(request):
